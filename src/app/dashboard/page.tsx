74732064111dfcb00d4869e01cd200691f9583ce
Daniel Sayer <daniel.sayer01@gmail.com>
2023-11-11 20:29:26 +1000

    Instantiate prisma with auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,22 @@
+import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
-import React from 'react'
 
 const Page = async () => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  })
+
+  if (!dbUser) {
+    redirect('/auth-callback?orgin=dashboard')
+  }
 
   return <div></div>
 }
