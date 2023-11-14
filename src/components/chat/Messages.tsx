4a6cef512945d230789eccbda59164f5a811123c
Daniel Sayer <daniel.sayer01@gmail.com>
2023-11-15 08:32:07 +1000

    Add stripe integration

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -79,7 +79,7 @@ const Messages = ({ fileId }: MessagesProps) => {
           <Skeleton className="h-16" />
         </div>
       ) : (
-        <div className="flex-1 flex flex-col` items-center justify-center gap-2">
+        <div className="flex-1 flex flex-col items-center justify-center gap-2">
           <MessageSquare className="h-8 w-8 text-violet-500" />
           <h3 className="font-semibold text-xl">You&apos;re all set!</h3>
           <p className="text-zinc-500 text-sm">
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,12 @@
-import { TRPCError } from '@trpc/server'
-import { privateProcedure, publicProcedure, router } from './trpc'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { db } from '@/db'
-import { z } from 'zod'
 import { INFINITE_QUERY_LIMIT } from '@/config/infiniteQuery'
+import { PLANS } from '@/config/stripe'
+import { db } from '@/db'
+import { getUserSubscriptionPlan, stripe } from '@/lib/stripe'
+import { absoluteUrl } from '@/lib/utils'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
+import { privateProcedure, publicProcedure, router } from './trpc'
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -159,6 +162,54 @@ export const appRouter = router({
         nextCursor,
       }
     }),
+  createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
+    const { userId } = ctx
+
+    const billingUrl = absoluteUrl('/dashboard/billing')
+
+    if (!userId) {
+      throw new TRPCError({ code: 'UNAUTHORIZED' })
+    }
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: userId,
+      },
+    })
+
+    if (!dbUser) {
+      throw new TRPCError({ code: 'NOT_FOUND' })
+    }
+
+    const subscriptionPlan = await getUserSubscriptionPlan()
+
+    if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: dbUser.stripeCustomerId,
+        return_url: billingUrl,
+      })
+
+      return { url: stripeSession.url }
+    }
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: billingUrl,
+      cancel_url: billingUrl,
+      payment_method_types: ['card'],
+      mode: 'subscription',
+      billing_address_collection: 'auto',
+      line_items: [
+        {
+          price: PLANS.find((plan) => plan.name === 'Pro')?.price.priceIds.test,
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId: userId,
+      },
+    })
+
+    return { url: stripeSession.url }
+  }),
 })
 
 export type AppRouter = typeof appRouter
