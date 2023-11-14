add0d06d041082e111ce8405a8f35ccfc896fbf0
Daniel Sayer <daniel.sayer01@gmail.com>
2023-11-14 21:05:49 +1000

    Add messages to app

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,32 +1,54 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
 import { Textarea } from '../ui/textarea'
 import { Button } from '../ui/button'
 import { Send } from 'lucide-react'
+import { ChatContext } from './ChatContext'
 
 interface ChatInputProps {
   isDisabled?: boolean
 }
 const ChatInput: React.FC<ChatInputProps> = ({ isDisabled }) => {
+  const { addMessage, handleInputChange, isLoading, message } =
+    useContext(ChatContext)
+
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex flex-col w-full flex-grow pt-4">
             <div className="flex items-center">
               <Textarea
+                ref={textAreaRef}
                 rows={1}
                 maxRows={4}
                 placeholder="Enter your question..."
                 autoFocus
                 className="resize-none me-2 text-base pt-3scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+                onChange={handleInputChange}
+                value={message}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' && !e.shiftKey) {
+                    e.preventDefault()
+                    addMessage()
+                    textAreaRef.current?.focus()
+                  }
+                }}
               />
-              <Button aria-label="send message">
+              <Button
+                aria-label="send message"
+                disabled={isLoading || isDisabled}
+                onClick={() => {
+                  addMessage()
+                  textAreaRef.current?.focus()
+                }}
+              >
                 <Send className="h-4 w-4" />
               </Button>
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   )
 }
--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -7,13 +7,13 @@ import Messages from './Messages'
 import { ChevronLeft, Loader2, XCircle } from 'lucide-react'
 import Link from 'next/link'
 import { buttonVariants } from '../ui/button'
+import { ChatContextProvider } from './ChatContext'
 
 interface ChatWrapperProps {
   fileId: string
 }
 
 const ChatWrapper: React.FC<ChatWrapperProps> = ({ fileId }) => {
-  console.log(fileId)
   const { data, isLoading } = trpc.getFileUploadStatus.useQuery(
     { fileId: fileId },
     {
@@ -85,13 +85,15 @@ const ChatWrapper: React.FC<ChatWrapperProps> = ({ fileId }) => {
   }
 
   return (
-    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
-      <div className="flex-1 flex flex-col justify-between mb-28">
-        <Messages />
-      </div>
+    <ChatContextProvider fileId={fileId}>
+      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className="flex-1 flex flex-col justify-between mb-28">
+          <Messages fileId={fileId} />
+        </div>
 
-      <ChatInput />
-    </div>
+        <ChatInput />
+      </div>
+    </ChatContextProvider>
   )
 }
 
